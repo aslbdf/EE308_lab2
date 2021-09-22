120298ef1db604bd785d3605f176d8ce15ee8dea
why <why@WHY>
2021-09-22 11:47:17 +0800

    添加项目文件。

---
+++ 软件工程lab2/file.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int mai() {
+    int i = 1;
+    double j = 0;
+    long f;
+    switch (i) {
+    case 0:
+        break;
+    case 1:
+        break;
+    case 2:
+        break;
+    default:
+        break;
+
+    }
+    switch (i) {
+    case 0:
+        break;
+    case 1:
+        break;
+    default:
+        break;
+
+    }
+    if (i < 0) {
+        if (i < -1) {}
+        else {}
+
+    }
+    else if (i > 0) {
+        if (i > 2) {}
+        else if (i == 2) {}
+        else if (i > 1) {}
+        else {}
+
+    }
+    else {
+        if (j != 0) {}
+        else {}
+
+    }
+    return 0;
+
+}
+
+
---
+++ 软件工程lab2/源.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+int main() {
+	string name;
+	ifstream cpp_file;
+	vector<string>keywords;
+	int keywords_length = 0;
+	string code_line = "";
+	cout << "please enter your file position: ";
+	//getline(cin, name);
+	cpp_file.open("file.cpp");
+	
+	
+	while (!cpp_file.eof()) {
+		getline(cpp_file, code_line);
+		int keychar_i = 0;
+		while (keychar_i < code_line.length()) {
+			if (!((code_line[keychar_i] >= 'a' && code_line[keychar_i] <= 'z') ||
+				(code_line[keychar_i] >= 'A' && code_line[keychar_i] <= 'Z') ||
+				code_line[keychar_i] == '.' || code_line[keychar_i] == '_'||
+				code_line[keychar_i] == '{' || code_line[keychar_i] == '}')) {
+				code_line[keychar_i] = ' ';
+			}
+			else if (code_line[keychar_i] == '{') {
+				code_line.insert(keychar_i + 1, " ");
+			}
+			keychar_i++;
+		}
+
+
+		code_line.append(" ;");
+		istringstream keyword_input(code_line);
+		string word;
+		string word2;
+		while (keyword_input >> word) {
+			word2 == "";
+			if (word == "else") {
+				keyword_input >> word2;
+				if (word2 == "if") {
+					word = word + word2;
+					keywords.push_back(word);
+					keywords_length++;
+				}
+				else {
+					keywords.push_back(word);
+					keywords.push_back(word2);
+					keywords_length++;
+					keywords_length++;
+				}
+			}
+			else {
+				keywords.push_back(word);
+				keywords_length++;
+			}
+		}
+	}
+
+
+
+
+	int keyword_i = 0;
+	while (keyword_i < keywords_length) {
+		if (keywords[keyword_i] == "int" || keywords[keyword_i] == "double" ||
+			keywords[keyword_i] == "long") {
+			keyword_i++;
+			while (keywords[keyword_i] != ";") {
+				string del_word = keywords[keyword_i];
+				if (del_word == "mai") {
+					keyword_i++;
+					continue; 
+				}
+				int del_i = 0;
+				while (del_i < keywords_length) {
+					if (keywords[del_i] == del_word) {
+						keywords.erase(begin(keywords) + del_i);
+						keywords_length--;
+					}
+					else {
+						del_i++;
+					}
+				}
+			}
+		}
+		keyword_i++;
+	}
+	int del_i = 0;
+	while (del_i < keywords_length) {
+		if (keywords[del_i] == ";") {
+			keywords.erase(begin(keywords) + del_i);
+			keywords_length--;
+		}
+		else { 
+			del_i++;
+		}
+	}
+		
+	for (int n = 0; n < keywords_length; n++) {
+		cout << keywords[n] << '\n';
+	}
+
+
+	int keywords_num = 0;
+	keyword_i = 0;
+	while (keyword_i < keywords_length) {
+		if (keywords[keyword_i] != "{" && keywords[keyword_i] != "}") {
+			keywords_num++;
+		}
+		keyword_i++;
+	}
+	cout << "keywords_num is " << keywords_num;
+	return 0;
+	
+}
